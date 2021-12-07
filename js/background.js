ad8235c8e45af4b5d74a68bc48791520a8aa9360
Hyeryeon Song <hyeryeon18@gmail.com>
2021-12-07 14:58:47 +0900

    TEST

---
+++ js/background.js
@@ -0,0 +1,19 @@
+const images = [
+  "01.jpg",
+  "02.jpg",
+  "03.jpg",
+  "04.jpg",
+  "05.jpg",
+  "06.jpg",
+  "07.jpg",
+  "08.jpg",
+];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)]; // randoem 이미지 선택
+
+const bgImage = document.createElement("img"); // img 태그 생성
+
+bgImage.src = `img/${chosenImage}`; // img 소스 지정
+
+document.body.appendChild(bgImage); // 생성한 bgImage를 body에 입력. append : body 제일 하단에 입력 / prepend : body 제일 상단에 입력
+console.log(bgImage);
